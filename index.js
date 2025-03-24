6f2305371a5d89f5561978a33bc787146ee01ae7
IndoOP <96644081+IndoOP@users.noreply.github.com>
2025-03-24 15:20:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const linkify = require('linkifyjs');
+const linkifyHtml = require('linkify-html');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
@@ -12,35 +14,35 @@ app.use(express.static(path.join(__dirname)));
 io.on('connection', socket => {
     console.log('A WS connection detected');
 
-    socket.emit('receive', 'RemoChat bot: Welcome to RemoChat');
 
     //name
     socket.on('joinRoom', (name) => {
         //broadcast
-    socket.broadcast.emit('receive', `RemoChat bot: A user user joined the chat`);
+        socket.broadcast.emit('receive', `RemoChat bot: ${name} joined the chat`);
 
-    //disconnect
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('receive', 'RemoChat bot: A user has left the chat');
-    })
+        //welcome
+        socket.emit('receive', `RemoChat bot: Welcome ${name} to RemoChat! Use !help for more info and !support to support us!`);
 
-    //chat message
-    socket.on('chatMessage', (msg) => {
-        console.log(msg);
-        socket.broadcast.emit('receive', `${name}: ${msg}`);
-        socket.emit('sending', `${name}: ${msg}`);
+        //disconnect
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('receive', `RemoChat bot: ${name} left the chat`);
+        })
 
-        if (msg.includes("!help") == true) {
+        //chat message
+        socket.on('chatMessage', (msg) => {
+            socket.broadcast.emit('receive', linkifyHtml(`<span style="font-weight: 800;">${name}</span>: ${msg}`));
+            socket.emit('sending', linkifyHtml(`<span style="font-weight: 800;">${name}</span>: ${msg}`));
+            if (msg.includes("!help") == true) {
                 io.emit('receive', `RemoChat Bot: This is a simple website for chatting there is nothing complicated however if you are facing any issues you can still contact us on our contact form which will be available soon!`)
-        }
+            }
 
-        if (msg.includes("!support") == true) {
+            if (msg.includes("!support") == true) {
                 io.emit('receive', `RemoChat Bot: Subscribe me on YouTube <a href="https://youtube.com/c/1ndo." target="blank" style="color: #730073;">LINK</a>`)
-        }
-    })
+            }
+        })
     })
 
-    
+
 })
 
 const PORT = 3000 || process.env.PORT;
